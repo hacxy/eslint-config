cc1564332512b94a6de48f70088d829b32fdf7bf
hacxy <hacxy.js@outlook.com>
2025-11-25 16:32:23 +0800

    feat: update dependencies and remove test script from CI

--- src/rules/react.ts
+++ src/rules/react.ts
@@ -1,8 +1,7 @@
 import type { Rules } from '@antfu/eslint-config';
 
 export const ReactRules: Rules = {
-  // 不允许使用propTypes
   'react/no-prop-types': [2],
-  // 无嵌套组件
   'react/no-nested-components': [2],
+  'react-hooks-extra/no-direct-set-state-in-use-effect': [2],
 };
