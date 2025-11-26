9912f48d5fddefa3e379e23172a3f3e1f9d060e2
hacxy <hacxy.js@outlook.com>
2025-11-26 14:22:21 +0800

    fix: update package.json and tsdown.config.ts for module exports and minification

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  minify: true,
   shims: true,
   format: ['esm'],
+  outExtensions: () => ({ js: '.js' }),
 });
